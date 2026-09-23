3d981eec5524304e234c6bcb5e10b0d5f56b4ff1
thuongr <thuongrejeehan@gmail.com>
2026-09-23 11:45:58 +1000

    qa: add validation helpers and launch checklist

---
+++ lib/validation.ts
@@ -0,0 +1,16 @@
+export function isValidEmail(value: string) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) && value.length <= 254;
+}
+
+export function isSafeInternalPath(value: string) {
+  return value.startsWith("/") && !value.startsWith("//") && !value.includes("\\");
+}
+
+export function escapeHtml(value: string) {
+  return value
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
